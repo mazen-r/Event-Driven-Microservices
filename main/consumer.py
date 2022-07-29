dbed0c607eb0b32213a7488d7f971f8f107b40b1
Mazen Ramadan <mazenramadan00@gmail.com>
2022-07-30 01:37:59 +0200

    improved data consistency

--- main/consumer.py
+++ main/consumer.py
@@ -1,5 +1,7 @@
 import pika
 
+from main import Product, db
+
 params = pika.URLParameters('amqps://wwwikjkc:pUD8_GzUi7O_w4cldiQyZVstok7LBxf4@toad.rmq.cloudamqp.com/wwwikjkc')
 
 connection = pika.BlockingConnection(params)
@@ -10,7 +12,25 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print ('Reveived in main')
+    data = json.loads(body)
     print (body)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+    
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
