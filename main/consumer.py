43064133f0d6f9479580ed683a83f569471744b3
Mazen Ramadan <mazenramadan00@gmail.com>
2022-07-30 01:09:48 +0200

    Consumer Service

--- main/consumer.py
+++ main/consumer.py
@@ -12,7 +12,7 @@ def callback(ch, method, properties, body):
     print ('Reveived in main')
     print (body)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print ('Started Cosuming')
 
