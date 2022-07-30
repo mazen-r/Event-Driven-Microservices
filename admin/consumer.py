1a640fd51995554ad80ace2498ff24cdc4dc1956
Mazen Ramadan <mazenramadan00@gmail.com>
2022-07-31 01:04:07 +0200

    final changes

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,4 +1,9 @@
-import pika
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import product
 
 params = pika.URLParameters('amqps://wwwikjkc:pUD8_GzUi7O_w4cldiQyZVstok7LBxf4@toad.rmq.cloudamqp.com/wwwikjkc')
 
@@ -10,7 +15,12 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
     print ('Reveived in admin')
-    print (body)
+    id = json.loads(body)
+    print (id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print ('product likes increased !')
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
